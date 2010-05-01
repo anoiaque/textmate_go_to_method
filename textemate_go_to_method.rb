5dbed11754da67046332aee67472781d27641b51
anoiaque <anoiaque@gmail.com>
2010-05-01 19:44:47 +0200

    README

--- textemate_go_to_method.rb
+++ textemate_go_to_method.rb
@@ -1,12 +1,12 @@
 #!/usr/bin/env ruby -w
 require File.join(ENV['TM_SUPPORT_PATH'], 'lib/textmate')
 
-def search_directory
-	File.join(ENV['TM_PROJECT_DIRECTORY'], 'app/*')
+def search_directories
+	[File.join(ENV['TM_PROJECT_DIRECTORY'], 'app/*'), File.join(ENV['TM_PROJECT_DIRECTORY'], 'lib/*')]
 end
 
 def go_to_method method
-  grep_return = `grep -rnE "def (self\.)?#{method}([\( ]{1}|$)" #{search_directory}`
+  grep_return = `grep -rnE "def (self\.)?#{method}([\( ]{1}|$)" #{search_directories.to_s}`
   filepath, line = grep_return.match(/^.+:[\d]+/).to_s.strip.split(':')
   TextMate.go_to(:file => filepath, :line => line)  if filepath && line
 end
